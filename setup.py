abd7e505218ea219d18a3573f48d793cf462f7e6
Denis <d.mulyalin@gmail.com>
2021-01-01 18:38:11 +1100

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __author__ = "Denis Mulyalin <d.mulyalin@gmail.com>"
 
 setup(
     name="salt_nornir",
-    version="0.2.1",
+    version="0.2.2",
     author="Denis Mulyalin",
     author_email="d.mulyalin@gmail.com",
     description="SALTSTACK Nornir Modules",
